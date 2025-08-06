931765afbd6af63e49604f2a375268b7f864ca03
jeffryacra <jeffryacra@gmail.com>
2025-08-05 22:06:37 -0700

    update

--- slider.js
+++ slider.js
@@ -3,9 +3,9 @@ const sliderValue = document.getElementById('sliderValue');
 const sliderDesc = document.getElementById('sliderDesc');
 
 const descs = [
-  { min: 0, max: 12, text: '“A medium cult”' },
-  { min: 13, max: 37, text: '“The entire family tree”' },
-  { min: 38, max: 50, text: '“Feeding the resistance”' }
+  { min: 5, max: 12, text: '"A medium cult"' },
+  { min: 13, max: 37, text: '"The entire family tree"' },
+  { min: 38, max: 50, text: '"Feeding the resistance"' }
 ];
 
 function updateSlider() {
